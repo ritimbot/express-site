547e679480e8b86f9f3c32f5453b74779ffb396e
erdem <erdemhso@mail.ru>
2020-10-06 20:46:52 +0300

    new urls and arrow func

--- index.js
+++ index.js
@@ -1,46 +1,24 @@
-const express = require("express");
-const app = express();
+const e = require("express");
+const web = e();
 
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/index.html");
-});
+// INDEX
+web.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
-app.get("/davet", (request, response) => {
-  response.redirect("https://discord.com/oauth2/authorize?client_id=711411719922253884&scope=bot&permissions=1110822208");
-});
+// URL's
+web.get("/davet", (req, res) => res.redirect("https://discord.com/oauth2/authorize?client_id=711411719922253884&scope=bot&permissions=1110822208"));
+web.get("/oy", (req, res) => res.redirect("https://top.gg/bot/711411719922253884/vote"));
+web.get("/youtube", (req, res) => res.redirect("https://www.youtube.com/channel/UCddBjStwXgfTOL2gvJ07_iA?"));
+web.get("/discord", (req, res) => res.redirect("https://discord.gg/9kdBEAj"));
+web.get("/github/:repo", (req, res) => res.redirect("https://github.com/ritimbot/"+req.params.repo));
+web.get("/sdn", (req, res) => res.redirect("https://forum.shiftdelete.net/konular/discord-icin-muzik-botu-ritim-bot.698307/"));
+web.get("/github", (req, res) => res.redirect("https://github.com/ritimbot/"));
+web.get("/patreon", (req, res) => res.redirect("https://www.patreon.com/ritim"));
+web.get("/dbl", (req, res) => res.redirect("https://top.gg/bot/711411719922253884/"));
+web.get("/api", (req, res) => res.redirect("https://api.ritim.xyz"));
+web.get("/twitter", (req, res) => res.redirect("https://twitter.com/ritimbot"))
 
-app.get("/oy", (request, response) => {
-  response.redirect("https://top.gg/bot/711411719922253884/vote");
-});
+// 404
+web.get("/404", (req, res) => res.sendFile(__dirname + "/404.html"));
+web.get("*", (req, res) => res.sendFile(__dirname + "/404.html"));
 
-app.get("/discord", (request, response) => {
-  response.redirect("https://discord.gg/9kdBEAj");
-});
-
-app.get("/github/:repo", (request, response) => {
-  response.redirect("https://github.com/ritimbot/"+request.params.repo)
-})
-
-app.get("/patreon", (request, response) => {
-  response.redirect("https://www.patreon.com/ritim");
-});
-
-app.get("/dbl", (request, response) => {
-  response.redirect("https://top.gg/bot/711411719922253884/");
-});
-
-app.get("/api", (request, response) => {
-  response.redirect("https://api.ritim.xyz");
-});
-
-app.get("/404", (request, response) => {
-  response.sendFile(__dirname + "/404.html");
-});
-
-app.get("*", (request, response) => {
-  response.sendFile(__dirname + "/404.html");
-});
-
-app.listen(process.env.PORT, function() {
-  console.log("ekspires dinlemeede!");
-});
+web.listen(process.env.PORT, () => console.log("ekspiress izlemede!"));
